d747a19e79c5b50a79da25c7ede4ad5eb217bea3
olawale micheal juwon <olawalejuwon@gmail.com>
2020-10-06 03:23:27 +0100

    https

--- app.js
+++ app.js
@@ -26,10 +26,24 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log('Connected correctly to the server');
-}, (err) => { console.log(err) });
+}, (err) => { 
+  console.log(err);
+  process.exit(1);
+ 
+ });
 
 
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
